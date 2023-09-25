48292d4d7785bca7d15f92cf073889c8acfa37e8
adr1an0s0ar3s <adrianosoares35@gmail.com>
2023-09-25 12:07:31 +0100

    feat: video starts where it's left off

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 document.addEventListener('keydown', function(event) {
     if(event.code == 'KeyE') {
         if (window.location.href.indexOf('watch') != -1) {
-            const videoId = (new URLSearchParams(window.location.search)).get('v');
-            window.location.href = 'https://www.youtube.com/embed/' + videoId + '?&autoplay=1';
+            const videoId = window.location.href.match(/(?<=(v=))[a-zA-Z0-9]+/)[0];
+            const currentTime = ~~document.getElementsByClassName('video-stream')[0].currentTime;
+            window.location.href = 'https://www.youtube.com/embed/' + videoId + '?autoplay=1&start=' + currentTime;
         } else if (window.location.href.indexOf('embed') != -1) {
-            const videoId = window.location.href.split('/').at(-1);
-            window.location.href = 'https://www.youtube.com/watch?v=' + videoId;
+            const videoId = window.location.href.match(/[a-zA-Z0-9]+((?=\?)|$)/)[0];
+            const currentTime = ~~document.getElementsByClassName('video-stream')[0].currentTime;
+            window.location.href = 'https://www.youtube.com/watch?v=' + videoId + '&t=' + currentTime;
         }
     }
 });
