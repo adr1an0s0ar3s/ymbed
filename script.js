941d8568731701eb745bd744111b4a669286a559
adr1an0s0ar3s <adrianosoares35@gmail.com>
2023-09-25 18:15:45 +0100

    bug: fix regex for some video ids

--- script.js
+++ script.js
@@ -5,11 +5,11 @@ document.addEventListener('keydown', function(event) {
 
     if(event.code == 'KeyE') {
         if (window.location.href.indexOf('watch') != -1) {
-            const videoId = window.location.href.match(/(?<=(v=))[a-zA-Z0-9]+/)[0];
+            const videoId = window.location.href.match(/(?<=(v=))[a-zA-Z0-9_-]*/)[0];
             const currentTime = ~~document.getElementsByClassName('video-stream')[0].currentTime;
             window.location.href = 'https://www.youtube.com/embed/' + videoId + '?autoplay=1&start=' + currentTime;
         } else if (window.location.href.indexOf('embed') != -1) {
-            const videoId = window.location.href.match(/[a-zA-Z0-9]+((?=\?)|$)/)[0];
+            const videoId = window.location.href.match(/[a-zA-Z0-9_-]*((?=\?)|$)/)[0];
             const currentTime = ~~document.getElementsByClassName('video-stream')[0].currentTime;
             window.location.href = 'https://www.youtube.com/watch?v=' + videoId + '&t=' + currentTime;
         }
